397232f5a1dd76b3741c7ef8184eba5ca7935e90
97fangz <briansegem25@gmail.com>
2022-09-08 20:25:55 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,20 +3,20 @@
 /**
  * main - Entry point
  * description: sizeof print
- *  Return: 0
+ * Return: 0
  */
 
 int main (void)
 {
-char achar;
-int aint;
+char acharacter;
+int ainteger;
 long along;
-long long along2;
+long long alonglong;
 float afloat;
 
-printf("size of achar: %lu bytes(s)\n",sizeof(achar));
-printf("size of aint: %lu bytes(s)\n",sizeof(aint));
+printf("size of achar: %lu bytes(s)\n",sizeof(acharacter));
+printf("size of aint: %lu bytes(s)\n",sizeof(ainteger));
 printf("size of along: %lu bytes(s)\n",sizeof(along));
-printf("size of along2: %lu bytes(s)\n",sizeof(along2));
+printf("size of along2: %lu bytes(s)\n",sizeof(alonglong));
 printf("size of afloat: %lu bytes(s)\n",sizeof(afloat));
 }
