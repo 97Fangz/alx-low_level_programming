50adaadba75b07c70e68d312993468426c256d28
97fangz <briansegem25@gmail.com>
2022-09-11 09:53:37 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,19 +4,17 @@
  * main - Entry point
  * description: prints alphabet in lowercase the uppercase
  * Return: 0
+ *
  */
+
 int main(void)
 {
 int alph;
-for (alph = 'a'; alph <= 'z'; alph++)
-{
-putchar(alph);
 
-for (alph = 'A'; alph <= 'Z'; alph++)
-{
+for (alph = 'a'; alph <= 'z'; alph++)
 putchar(alph);
-}
-}
+for (alph = 'A'; alph <= 'Z'; alph++)
+	putchar(alph);
 putchar('\n');
 return (0);
 }
