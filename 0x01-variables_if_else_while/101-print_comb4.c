49d344e516c80487b004a4b93e00a6b5443dcaa2
97fangz <briansegem25@gmail.com>
2022-09-12 07:13:40 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,17 +9,17 @@
 int main(void)
 {
 	int i, j, k;
-for (i = 0; i <= 7; i++)
+for (i = 48; i <= 55 ; i++)
 {
-for (j = 1; j <= 8; j++)
+for (j = 49; j <= 56; j++)
 {
-for (k = 2; k <= 9; k++)
+for (k = 50; k <= 57; k++)
 	if (k > j && j > i)
 	{
 	putchar(i);
 	putchar(j);
 	putchar(k);
-	if (i != 7 || j != 8 || k != 9)
+	if (i != 55 || j != 56 || k != 57)
 	{
 	putchar(',');
 	putchar(' ');
