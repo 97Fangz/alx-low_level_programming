aad24e4aa61109fc8d4cd6e5613eecff41d8b01b
97fangz <briansegem25@gmail.com>
2022-09-12 00:06:06 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,17 +9,15 @@
 int main(void)
 {
 int n;
-int ch;
+char ch;
 
 for (n = 0; n < 10; n++)
 {
 putchar(n);
-
+}
 for (ch = 'a'; ch <= 'f'; ch++)
 {
 putchar(ch);
-}
-
 }
 putchar('\n');
 return (0);
