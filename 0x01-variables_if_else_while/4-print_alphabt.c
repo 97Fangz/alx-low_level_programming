38634745c579168fc5aa162aa37baddc009c39d8
97fangz <briansegem25@gmail.com>
2022-09-11 18:24:54 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * description: prints all letters except 'q' and 'e'
+ * Return: 0
+ */
+
+int main(void)
+{
+	int alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+	if (alph == 'e'  || alph == 'q' )
+		continue;
+putchar(alph);
+putchar('\n');
+return (0);
+}
