32831eab41fbd1f1efc25d0bcab8e2398d8ec4be
97fangz <briansegem25@gmail.com>
2022-09-11 18:42:40 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main -Entry Point
+ * description: prints all single base 10 digits
+ * Return: 0
+ */
+
+int main(void)
+{
+int btn;
+for (btn = 0; btn < 10; btn++)
+{
+putchar(btn);
+}
+putchar('\n');
+return (0);
+}
