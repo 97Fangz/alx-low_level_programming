2bdf3db78d437d8399e5298e2c285b9c6faabf0b
97fangz <briansegem25@gmail.com>
2022-09-16 06:59:29 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,13 @@ void times_table(void)
 {
 int i, j;
 
+
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
 printf("%2d", i * j);
-if (j != 9)
+if (i != 9 && j != 9)
 {
 _putchar(',');
 _putchar(' ');
