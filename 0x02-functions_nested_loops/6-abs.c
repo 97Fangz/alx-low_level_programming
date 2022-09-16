615cec7771b73a589332ae02fd007286cf52dac9
97fangz <briansegem25@gmail.com>
2022-09-16 01:47:46 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * description: 
+ *
  * Return: n if positive else return -n
  */
 
 int _abs(int n)
 {
-if (n > 0)
+if (n >= 0)
 	return (n);
 else
 	return (-n);
