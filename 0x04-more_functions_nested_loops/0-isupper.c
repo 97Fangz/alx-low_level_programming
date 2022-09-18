a8fc1f5c1d0221a33193f9ec12299aaec2aaa03c
97fangz <briansegem25@gmail.com>
2022-09-18 14:37:26 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
+/**
+ * _isupper - entry point
+ * @c: character to be checked
+ * Return:1 if true else return 0
+ */
+
 int _isupper(int c)
 {
-if (c >= 0 && c <= 9)
-{
-if  (c == 2 && c == 4)
-{
-continue;
-}
-_putchar(c);
-}
-_putchar('\n')
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
