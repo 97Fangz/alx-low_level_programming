1f1e2637e75ece572c20c23048958980d7d86954
97fangz <briansegem25@gmail.com>
2022-09-19 12:50:18 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ * from zero to forteen
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+
+	int i;
+
+	for (c = 0;c < 10; c++)
+	{
+	if (c != 2 && c != 4)
+	{
+	_putchar(c + '0');
+	}
+	}
+	_putchar('\n');
+}
