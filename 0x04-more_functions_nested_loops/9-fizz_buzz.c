8560e09f6bae9c183eafadf29ff2246fe7fe004d
97fangz <briansegem25@gmail.com>
2022-09-21 01:33:31 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,21 +18,22 @@ int main(void)
 	{
 	if (i % 3 == 0)
 	{
-	printf("Fizz");
+	printf(" Fizz");
 	}
 	else if (i % 5 == 0)
 	{
-	printf("Buzz");
+	printf(" Buzz");
 	}
 	else if (i % 3 == 0 && i % 5 == 0)
 	{
-	printf("FizzBuzz");
+	printf(" FizzBuzz");
 	}
 	else
 	{
 	printf("%d", i);
 	}
 	}
-	printf("Buzz\n");
-	return 0;
+	printf('\n');
+
+	return (0);
 }
