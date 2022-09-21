1790a7d12e411defde9971669a61f1031a2d38a5
97fangz <briansegem25@gmail.com>
2022-09-21 03:36:55 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - checks for a digit 0 - 9
+ * description: finds a prime factor
+ * Return: 0
+ */
+
+int main(void)
+{
+long x, y;
+
+x = 612852475143;
+
+for (y = 2; x> y; y++)
+{
+while(x %y == 0)
+{
+x = x / y;
+}
+}
+printf("%lu", y);
+putchar('\n');
+return (0);
+}
