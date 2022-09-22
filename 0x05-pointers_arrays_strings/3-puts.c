fe5beea463ab512dfbb08f80405762feec5bf67e
97fangz <briansegem25@gmail.com>
2022-09-22 08:30:43 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string 
+ * followed by a new line
+ * @str: character checked
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+int c;
+
+for (c = 0; str[c] != '\0'; c++)
+{
+	_putchar(str[c]);
+}
+
+}
