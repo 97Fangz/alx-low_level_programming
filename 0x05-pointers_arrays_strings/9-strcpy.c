909de14d222c1e842dbcaa532a1f961d8ca6238a
97fangz <briansegem25@gmail.com>
2022-09-23 04:59:58 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * strcpy - entry point
+ * @dest: first integer
+ * @scr: second integer
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0, j = 0;
+
+while(src[i] != '\0')
+{
+i++;
+}
+
+for (; j < i; j++)
+{
+dest[j] = src[j];
+}
+dest [j] = '\0';
+
+return (dest);
+}
