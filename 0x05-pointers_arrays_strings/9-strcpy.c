833087774c2ea09a9617beb0bdbe733db4906af7
97fangz <briansegem25@gmail.com>
2022-09-23 05:02:26 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcpy - entry point
+ * _strcpy - entry point
  * @dest: first integer
  * @scr: second integer
  * Return: 0
@@ -11,7 +11,7 @@ char *_strcpy(char *dest, char *src)
 {
 int i = 0, j = 0;
 
-while(src[i] != '\0')
+while (src[i] != '\0')
 {
 i++;
 }
