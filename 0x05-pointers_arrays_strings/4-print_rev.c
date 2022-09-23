a6e5d60a1a5ef7589b991355a9c6be7aab23eb2d
97fangz <briansegem25@gmail.com>
2022-09-22 23:50:23 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,35 @@
 #include "main.h"
 
 /**
+ * _strlen - swaps the values of two integers
+ * @s: string input
+ * Return: length of string
+ */
+
+/**
  * print_rev - prints a strin in reverse
- * @s: character checked
+ * @s: string input
  * Return: 0
  */
+int _strlen(char *s)
+{
+int i = 0;
+
+while(s[i])
+{
+i++;
+}
+return (i);
+}
 
 void print_rev(char *s)
 {
-int i;
+int i = _strlen(s) - 1;
 
-for(i = 0; s[i] !=0; i++)
+while (s[i])
 {
-_putchar (s[--i]);
+_putchar(s[i]);
+i--;
 }
 _putchar('\n');
 }
