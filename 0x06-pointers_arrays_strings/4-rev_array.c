0263cbed115a6953e74ca596fba8f7cf101d116f
97fangz <briansegem25@gmail.com>
2022-09-28 02:45:37 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * reverse_array - entry point
+ * description: reverses the content of an array
+ * @a: first character
+ * @n: second character
+ * Return: 0
+ */
+
 void reverse_array(int *a, int n)
 {
 int reverse, i, j;
