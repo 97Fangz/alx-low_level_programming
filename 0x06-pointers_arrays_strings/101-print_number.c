82aecc219d6043a6db9897063b5568e4eaf5269f
97fangz <briansegem25@gmail.com>
2022-09-28 04:52:26 -0700

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input integer
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+unsigned int i;
+
+if (i < 0)
+{
+i = -i;
+}
+else
+{
+i = i;
+}
+if (i / 10)
+	print_number(i / 10);
+
+	_putchar((i % 10) + '0');
+}
