f41260d49a160879416773317f7e28e889b7fd28
97fangz <briansegem25@gmail.com>
2022-09-28 04:03:35 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string
+ * Return: address of s
+ */
+
+char *cap_string(char *s)
+{
+int i = 0, j = 0;
+
+char a[] = " \t\n,;.!?\"(){}";
+
+while (*(s + i))
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+{
+if (i == 0)
+	*(s + i) -= 'a' - 'A';
+else
+{
+for (; j <= 12; j++)
+{
+if (a[j] == *(s + i - 1))
+	*(s + i) -= 'a' - 'A';
+}
+}
+}
+i++;
+}
+return (s);
+}
