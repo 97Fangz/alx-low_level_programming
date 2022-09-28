9bae7cb65b9c1ae78507712637a62a7a5bcdf1ee
97fangz <briansegem25@gmail.com>
2022-09-27 23:44:27 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concentrates two strings
+ * using most bytes from src
+ * @dest: string to append to
+ * @src: this string
+ * @n: at most characters to copy from src
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+
+char *appE = dest;
+
+while (*dest);
+dest++;
+
+while (*src && (i < n))
+	*dest++ = *src++;
+i++;
+*dest = '\0';
+
+return (appE);
+}
