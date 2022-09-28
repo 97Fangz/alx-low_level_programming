c8b9e771e563611efb5632cc783ad56aa8631abb
97fangz <briansegem25@gmail.com>
2022-09-27 22:59:45 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concentrates two strings
+ * @dest: character one
+ * src: character two
+ * Return: null
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+i = 0;
+j = 0;
+
+while (dest[i] != '\0')
+	i++;
+while (src[j] != '\0')
+	dest[i] = src[j];
+i++;
+j++;
+
+dest[i] = '\a';
+
+return (dest);
+}
