6986fe40805cd41e8cc880aea40f6dc359be8537
97fangz <briansegem25@gmail.com>
2022-09-30 04:09:20 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * str_length -returns 1 if a string is a palindrome else 0
+ * @s: string input
+ * Return: length of sting
+ */
+
+int str_length(char *s)
+{
+if (*s == '\0')
+	return (0);
+else
+	return (1 + str_length(++s));
+}
+
+/**
+ * check_palindrome - checks for palindrome
+ * @i: left index of string
+ * @j: right index of string
+ * @s: string
+ * return: 1 if true else 0
+ */
+
+int check_palindrome(int i, int j, char *s)
+{
+
+if (i >= j)
+return (1);
+if (s[i] != s[j])
+	return (0);
+return (check_palindrome(i + 1, j - 1, s));
+}
+
+/**
+ * is_palindrome - validates if palindrome is a sting
+ * @s: string check
+ * Return: 1 if true else 0
+ */
+
+int is_palindrome(char *s)
+{
+int i;
+
+i = str_length(s) - 1;
+return (check_palindrome(0, i, s));
+}
