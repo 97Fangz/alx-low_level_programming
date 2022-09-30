67fe4dc9e08fb5beac4dccaf915d79d81e9823d4
97fangz <briansegem25@gmail.com>
2022-09-30 02:53:21 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * sqrt_check - recursively checks the square of n
+ * @n: character input
+ * @inpt: increment square root of n
+ * Return: 0
+ */
+int sqrt_check(int n, int inpt)
+{
+if (inpt == n)
+	return(inpt);
+else if (inpt * inpt > n)
+	return (-1);
+return(sqrt_check(n, inpt + 1));
+}
+/**
+ * _sqrt_recursion -returns the natural square of a number
+ * @n: input integer
+ * Return: square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+return (sqrt_check(n, 0));
+}
