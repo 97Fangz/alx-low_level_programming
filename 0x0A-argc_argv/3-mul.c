5df23517278c5579a31a346a5ccfd8a6f9c0f864
97fangz <briansegem25@gmail.com>
2022-10-11 04:29:38 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * description: if it does not receive two argiuments,print Error - return 1
+ * @argc: number of command line arguments
+ * @argv: list of command line arguments
+ * Return: 0 else 1
+ */
+
+int main(int argc, char **argv)
+{
+if (argc !=3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
