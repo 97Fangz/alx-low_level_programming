fbd48037d4e74e717d4ce62c4582378d5193dbfd
97fangz <briansegem25@gmail.com>
2022-10-11 21:17:13 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,31 +4,31 @@
 /**
  * main - adds positive numbers
  * description: if no number is passed to the program print 0
- * print Error if one of the number contains symbols that are not digits - return 1
+ * print Error if one of the number contains symbols
+ * that are not digits - return 1
  * @argc: number of commandline arguments
  * @argv: list of command line arguments
  * Return: 0 else 1
  */
 
- int main(int argc, char ** argv)
- {
+int main(int argc, char **argv)
+{
 	 int sum = 0, i, j;
 	 char *ptr;
-
- if (argc > 1)
- {
- for (i = 1; i < argc; i++)
- {
- j = strtol(argv[i], &ptr, 10);
- if (!*ptr)
-	 sum += j;
- else
- {
- printf("Error\n");
- return (1);
- }
- }
- }
- printf("%d\n", sum);
- return (0);
- }
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+j = strtol(argv[i], &ptr, 10);
+if (!*ptr)
+sum += j;
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+printf("%d\n", sum);
+return (0);
+}
