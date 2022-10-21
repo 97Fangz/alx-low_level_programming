6689e835f5728e94a425d153e7efa62bfd66046c
97fangz <briansegem25@gmail.com>
2022-10-21 09:00:36 -0700

    6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - function that computes the absolute value of
+ * an integer n
+ * @n: int
+ * Return: @n value
+ */
+
+int _abs(int n)
+{
+return (abs(n));
+}
