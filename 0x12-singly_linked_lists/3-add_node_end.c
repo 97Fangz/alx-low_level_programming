a84efa769ce2a4651d431afc4953a3f92187b5fb
97fangz <briansegem25@gmail.com>
2022-10-26 07:52:14 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -13,39 +12,30 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new;
-list_t *temp;
+list_t *temp = *head;
+unsigned int len = 0;
+
+while (str[len])
+	len++;
 
 new = malloc(sizeof(list_t));
 if (!new)
 	return (NULL);
-temp = *head;
+
 new->str = strdup(str);
-new->len = strlen(str);
+new->len = len;
 new->next = NULL;
+
 if (*head == NULL)
 {
 *head = new;
 return (new);
 }
-while (temp->next != NULL)
-{
+
+while (temp->next)
 	temp = temp->next;
+
 	temp->next = new;
+
 	return (new);
-}
-}
-
-/**
- * _strlen - calculates the length of a string
- * @s: string
- * Return: length of the string
- */
-
-int _strlen(const char *s)
-{
-int idx = 0;
-
-for (; s[idx]; idx++)
-	;
-return (idx);
 }
