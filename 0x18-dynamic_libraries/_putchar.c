ebde0675ee01f125ed6c406cdc3ccf2ab26ea925
97fangz <briansegem25@gmail.com>
2022-12-26 07:25:55 -0800

    dynamic_libraries

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,13 @@
+# include <unistd.h>
+
+/**
+ * _putchar - Entry point
+ * @c: main character
+ * Description: prints out _putchar
+ * Return: Always 0 (success)
+ */
+
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
